fe004639c8ca0ea9f0cc9c3f31fb6617b06d68bb
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-06-17 13:33:55 +0200

    Add support for replacing snapx token during build.

--- build.ps1
+++ build.ps1
@@ -4,14 +4,19 @@ param(
     [Parameter(Position = 1, ValueFromPipeline)]
     [ValidateSet("Debug", "Release")]
     [string] $Configuration = "Release",
-    [Parameter(Position = 0, ValueFromPipeline)]
-    [switch] $Nupkg
+    [Parameter(Position = 2, ValueFromPipeline)]
+    [switch] $Nupkg,
+    [Parameter(Position = 3, ValueFromPipeline)]
+    [string] $SnapxToken = $null
 )
 
 $WorkingDirectory = Split-Path -parent $MyInvocation.MyCommand.Definition
 . $WorkingDirectory\common.ps1
 
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+
 $BuildOutputDirectory = Join-Path $WorkingDirectory build\$Version
+$SnapxYmlFilenamePath = Join-Path $WorkingDirectory .snapx\snapx.yml
 
 Resolve-Shell-Dependency dotnet
 
@@ -22,3 +27,8 @@ Invoke-Command-Colored dotnet @(
     "--output $BuildOutputDirectory"
     "--configuration $Configuration"
 )
+
+if($SnapxToken -ne $null) {
+    $SnapxYmlContent = (Get-Content $SnapxYmlFilenamePath) -replace "token:.*", "token: ${SnapxToken}"
+    $SnapxYmlContent | Out-File $SnapxYmlFilenamePath -Encoding $Utf8NoBomEncoding
+}
