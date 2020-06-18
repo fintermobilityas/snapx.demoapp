63b9ef2bb9a256ee6cbd6a8866c5261fe061b2a7
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-06-18 14:55:19 +0200

    Update shell scripts as this repository is no longer a submodule of the snapx project.

--- snapx-dev.cmd
+++ snapx-dev.cmd
@@ -1,2 +1,2 @@
-dotnet build -c Debug ../Snapx/Snapx.csproj 
-dotnet ../Snapx/bin/Debug/netcoreapp3.1/Snapx.dll %*
+dotnet build -c Debug ../snapx/src/Snapx/Snapx.csproj 
+dotnet ../snapx/src/Snapx/bin/Debug/netcoreapp3.1/Snapx.dll %*
