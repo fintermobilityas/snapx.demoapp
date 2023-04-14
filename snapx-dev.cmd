0eabb3c553995185e59b63430b7340690c341d52
Peter Rekdal Khan-Sunde <peter.sunde@gmail.com>
2023-04-14 13:02:56 +0200

    Nuke unused files.

--- snapx-dev.cmd
+++
@@ -1,2 +0,0 @@
-dotnet build -c Debug ../snapx/src/Snapx/Snapx.csproj 
-dotnet ../snapx/src/Snapx/bin/Debug/net5.0/Snapx.dll %*
--- snapx-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dotnet build -c Debug ../snapx/src/Snapx.csproj
-dotnet ../snapx/src/Snapx/bin/Debug/net5.0/snapx.dll $*
